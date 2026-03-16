dc5b44f4e2f7efa7371eb7ae13bdffb7a312d291
drp369-369 <pauldeep1212@gmail.com>
2026-03-16 11:42:19 +0530

    feat: add ranking engine to compare three inputs

---
+++ ranker.py
@@ -0,0 +1,9 @@
+# ranker.py
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+largest = max(a, b, c)
+
+print("Largest number is:", largest)
